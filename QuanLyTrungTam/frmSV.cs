597cd13e3dbb2accd2e9ec02ce75b5d633965b20
cong04122003 <conggo2003@gmail.com>
2023-12-04 21:36:35 +0700

    Fix
    Thêm nút xóa vs tìm kiếm cho form HV

--- QuanLyTrungTam/frmSV.cs
+++ QuanLyTrungTam/frmSV.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuanLyTrungTam
 {
@@ -16,14 +17,23 @@ namespace QuanLyTrungTam
         {
             InitializeComponent();
         }
-      
-
+        string connectionString;
+        SqlConnection conn = new SqlConnection();
+        DataSet ds;
+        SqlDataAdapter da;
+        private string tukhoa = "";
         private void frmSV_Load(object sender, EventArgs e)
         {
             LoadHV();
         }
         private void LoadHV()
-        {
+        {   //khaibao listCustomParameter
+            List<CustomParameter> lstPara = new List<CustomParameter>();
+            lstPara.Add(new CustomParameter()
+            {
+                key = "@tukhoa",
+                value = tukhoa
+            });
             dgvsinhvien.DataSource = null; // reset lai csdl
             //load toan bo danh sach hoc vien khi form duoc load
             //khai bao list custom parameter 
@@ -39,10 +49,13 @@ namespace QuanLyTrungTam
         }
         private void dgvsinhvien_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {   //Lay msv
-            var mhv = dgvsinhvien.Rows[e.RowIndex].Cells["MaHocVien"].Value.ToString();
-            new frmsinhviencon(mhv).ShowDialog();
-            //Sau khi form hoc vien dong lai can load lai danh sach hoc vien
-            LoadHV();
+            if (e.RowIndex >= 0)
+            {
+                var mhv = dgvsinhvien.Rows[e.RowIndex].Cells["MaHocVien"].Value.ToString();
+                new frmsinhviencon(mhv).ShowDialog();
+                //Sau khi form hoc vien dong lai can load lai danh sach hoc vien
+                LoadHV();
+            }
         }
         private void btnthem_Click(object sender, EventArgs e)
         {   //neu them moi sinh vien thi ma sinh vien la null
@@ -53,6 +66,47 @@ namespace QuanLyTrungTam
         private void dgvsinhvien_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//xoahocvien co gi nho check lai doan MaHocVien
+            int CurrentIndex = dgvsinhvien.CurrentCell.RowIndex;
+            var mathuoc = dgvsinhvien.Rows[CurrentIndex].Cells["MaHocVien"].Value.ToString();
+            if (MessageBox.Show("Bạn có chắc muốn xóa Học Viên này?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                string ma = mathuoc.ToString();
+                List<CustomParameter> lstpara = new List<CustomParameter>();
+                string sql = "deleteHV";
+                {
+
+                    lstpara.Add(new CustomParameter()
+                    {
+                        key = "@MaHocVien",
+                        value = ma
+                    });
+                }
+                var rs = new database().Excute(sql, lstpara);
+                if (rs == 1)
+                {
+                    if (string.IsNullOrEmpty(ma))
+                    {
+                        MessageBox.Show("Xóa Học Viên Thành Công!", "Thông Báo");
+                        LoadHV();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Xóa Giảng Viên Thất Bại!", "Thông Báo");
+                }
+
+            }
+            LoadHV();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//timkiem hoc vien
+            tukhoa = txttimkiem.Text;
+            LoadHV();
         }
     }
 }
